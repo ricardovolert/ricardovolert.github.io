24385eee556d332b80945fe7fa12e308081e52f3
Ricardo Volert <76571617+ricardovolert@users.noreply.github.com>
2024-05-20 09:48:54 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const changeColorButton = document.getElementById("changeColorButton");
-    
-    changeColorButton.addEventListener("click", function() {
-        const colors = ["#ff5733", "#33ff57", "#5733ff", "#33fff3", "#f333ff"];
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        document.body.style.backgroundColor = randomColor;
-    });
-});
